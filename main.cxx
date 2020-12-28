b90d10132eee8f5e3582e9a8bcb50363f5b821d4
leobersi <67894504+leobersi@users.noreply.github.com>
2020-12-28 16:01:46 +0100

    Added new methods of particles generation
    Work

--- main.cxx
+++ main.cxx
@@ -17,17 +17,17 @@ int main() {
   gStyle->SetOptTitle(0);
   ///////////////////////////////////////////////
   gRandom->SetSeed();
-  constexpr int nGen = 1e4; //events
+  constexpr int nGen = 1e4; //events 1e05!!
   constexpr int nParticle = 100; //number of particles generated per event
   constexpr int N = 130; //dimension of static array
 
-  Particle::AddParticleType("P+", 0.13957, +1);
-  Particle::AddParticleType("P-", 0.13957, -1);
-  Particle::AddParticleType("K+", 0.49367, +1);
-  Particle::AddParticleType("K-", 0.49367, -1);
-  Particle::AddParticleType("p+", 0.93827, +1);
-  Particle::AddParticleType("e-", 0.93827, -1);
-  Particle::AddParticleType("K*", 0.89166, 0, 0.050);
+  Particle::AddParticleType("P+", 0.13957, +1); //0
+  Particle::AddParticleType("P-", 0.13957, -1); //1
+  Particle::AddParticleType("K+", 0.49367, +1); //2
+  Particle::AddParticleType("K-", 0.49367, -1); //3
+  Particle::AddParticleType("p+", 0.93827, +1); //4
+  Particle::AddParticleType("e-", 0.93827, -1); //5
+  Particle::AddParticleType("K*", 0.89166, 0, 0.050); //6
   Particle particle[N];
 
   TFile *file = new TFile("/home/leonardo/root-build/macros/my_testroot.root","RECREATE"); //CAMBIA DIRECTORY ALLA FINE
@@ -44,7 +44,7 @@ int main() {
          TH1F *mass3  =  new TH1F("mass3", "Invariant Mass P and K discord charge", 160, 0, 2);
          TH1F *mass4  =  new TH1F("mass4", "Invariant Mass P and K concord charge", 160, 0, 2);
          TH1F *mass5  =  new TH1F("mass5", "Invariant Mass couples decayed from K*", 160, 0, 2);
-         TH1F *mass6  =  new TH1F("mass6", "Invariant Mass all particles generated", 160, 0,2);
+         TH1F *mass6  =  new TH1F("mass6", "Invariant Mass all particles generated", 160, 0, 2);
 
   double Theta, Phi, Impulse, Px, Py, Pz, x = 0;
 
@@ -59,9 +59,9 @@ int main() {
       Impulse = gRandom->Exp(1);
       impulse->Fill(Impulse);
 
-      Pz = Impulse * cos(Theta);
       Px = Impulse * sin(Theta) * cos(Phi);
       Py = Impulse * sin(Theta) * sin(Phi);
+      Pz = Impulse * cos(Theta);
       trasv_imp->Fill(sqrt(Px * Px + Py * Py));
 
       particle[j].Set_P(Px, Py, Pz);
@@ -79,31 +79,23 @@ int main() {
         particle[j].Set_indexParticle(4);
       } else if (x < 0.99) {
         particle[j].Set_indexParticle(5);
-      } else if (x < 0.995) {
-        particle[j].Set_indexParticle(6);
-
-        particle[100 + decays_counter].Set_indexParticle(0);
-        particle[100 + decays_counter + 1].Set_indexParticle(3);
-        particle[j].Decay2body(particle[100 + decays_counter],
-                               particle[100 + decays_counter + 1]);
-        mass5->Fill(particle[100 + decays_counter].InvMass(particle[100 + decays_counter + 1]));
-        decays_counter++;
-        decays_counter++;
       } else {
         particle[j].Set_indexParticle(6);
-
-        particle[100 + decays_counter].Set_indexParticle(2);
-        particle[100 + decays_counter + 1].Set_indexParticle(1);
-        particle[j].Decay2body(particle[100 + decays_counter],
-                               particle[100 + decays_counter + 1]);
-        mass5->Fill(particle[100 + decays_counter].InvMass(particle[100 + decays_counter + 1]));
+        x = gRandom->Rndm();
+          if(x < 0.5) {
+          particle[100 + decays_counter].Set_indexParticle(0);
+          particle[100 + decays_counter + 1].Set_indexParticle(3);
+          particle[j].Decay2body(particle[100 + decays_counter], particle[100 + decays_counter + 1]);
+          } else {
+            particle[100 + decays_counter].Set_indexParticle(1);
+            particle[100 + decays_counter + 1].Set_indexParticle(2);
+            particle[j].Decay2body(particle[100 + decays_counter], particle[100 + decays_counter + 1]);
+            } 
         decays_counter++;
         decays_counter++;
       }
-
       if (particle[j].Get_indexParticle() >= 0) {
-        types->Fill(
-            particle[j].Get_indexParticle()); // filling of generated particles
+        types->Fill(particle[j].Get_indexParticle()); // filling of generated particles
       }
     }
 
@@ -111,27 +103,27 @@ int main() {
       energy->Fill(particle[j].Energy()); // filling of generated energies
     }
 
-    for (int j = 0; j < 100 + decays_counter; ++j) {
+    for (int j = 0; j < 100 + decays_counter-1; ++j) {
       for (int h = j + 1; h < 100 + decays_counter; h++) {
         mass6->Fill(particle[j].InvMass(particle[h])); // filling mass6
+        //if(j < nParticle && h < nParticle)
         if (particle[j].Get_Charge() != particle[h].Get_Charge()) {
             mass1->Fill(particle[j].InvMass(particle[h])); // filling mass1
           }
         if (particle[j].Get_Charge() == particle[h].Get_Charge()) {
             mass2->Fill(particle[j].InvMass(particle[h])); // filling mass2
           }
-        if ((particle[j].Get_indexParticle() == 0 &&
-             particle[h].Get_indexParticle() == 3) ||
-            (particle[j].Get_indexParticle() == 1 &&
-             particle[h].Get_indexParticle() == 2)) {
+        if ((particle[j].Get_indexParticle() == 0 && particle[h].Get_indexParticle() == 3) || //P+ & K-
+            (particle[j].Get_indexParticle() == 1 && particle[h].Get_indexParticle() == 2)) { //P- & K+
           mass3->Fill(particle[j].InvMass(particle[h])); // filling mass3
         }
-        if ((particle[j].Get_indexParticle() == 1 &&
-             particle[h].Get_indexParticle() == 3) ||
-            (particle[j].Get_indexParticle() == 0 &&
-             particle[h].Get_indexParticle() == 2)) {
+        if ((particle[j].Get_indexParticle() == 1 && particle[h].Get_indexParticle() == 3) || //P- & K-
+            (particle[j].Get_indexParticle() == 0 && particle[h].Get_indexParticle() == 2)) { //P+ & K+
           mass4->Fill(particle[j].InvMass(particle[h])); // filling mass4
         }
+        if((j >= nParticle && h >= nParticle) && (j%2 == 0 && h == j+1)) { // couple of decayed particles 
+					mass5-> Fill(particle[j].InvMass(particle[h]));
+				}  
       }
     }
   }
