313627121c246017acc30cfc153d57cec4aa4c5d
leobersi <67894504+leobersi@users.noreply.github.com>
2020-12-29 19:38:08 +0100

    Pad Added. Best cosmetics

--- Macroloader.C
+++ Macroloader.C
@@ -10,6 +10,7 @@ void OperationHisto() {
   if ( file->IsOpen() ) cout << "File opened successfully" << '\n';
 
   //retrieving histograms
+  TH1F* typesGenerated = (TH1F*)file->Get("types");
   TH1F* hTheta = (TH1F*)file->Get("correlationT");
   TH1F* hPhi = (TH1F*)file->Get("correlationP");
   TH1F* hImpulse = (TH1F*)file->Get("impulse");
@@ -28,9 +29,8 @@ void OperationHisto() {
    subtraction_invMass->Add(invMass_discord,invMass_concord,1,-1);
    subtraction_invMass->SetTitle("Subtraction of all discord and concord Invariant Mass");
    //fitting
-   hTheta->Fit("pol0","Q","0");
-   hPhi->Fit("pol0","Q","0");
-   hImpulse->Fit("expo","Q","0");
+   hTheta->Fit("pol0","Q");
+   hPhi->Fit("pol0","Q");
    
    subtraction_invMass_PK->Fit("gaus","R","",0.6,1.2);
    subtraction_invMass->Fit("gaus","R","",0.6,1.2);
@@ -39,91 +39,146 @@ void OperationHisto() {
    subtraction_invMass_PK->SetEntries(subtraction_invMass_PK->Integral());
    subtraction_invMass->SetEntries(subtraction_invMass->Integral());
    
-   //create First canvas1
-   TCanvas *canvas1 = new TCanvas("canvas1","Generations",200,10,700,900);
-   canvas1->SetFillColor(18);
-   canvas1->Divide(3);
-   canvas1->SetGridx();
-   canvas1->SetGridy();
-   
-   //canvas1 first part
-   canvas1->cd(1);
-   canvas1->cd(1)->SetFillColor(42);
-   gStyle->SetOptFit(111);
-   hTheta->DrawCopy();
-   //TCanvas::Update()
-   canvas1->cd(1)->Update();
-   canvas1->cd(1)->GetFrame()->SetFillColor(21);
-   canvas1->cd(1)->Modified();
-   //canvas2 second part
-   canvas1->cd(2);
-   canvas1->cd(2)->SetFillColor(42);
-   gStyle->SetOptFit(111);
-   hPhi->DrawCopy();
-   //TCanvas::Update()
-   canvas1->cd(2)->Update();
-   canvas1->cd(2)->GetFrame()->SetFillColor(21);
-   canvas1->cd(2)->Modified();
-   //canvas2 third part
-   canvas1->cd(3);
-   canvas1->cd(3)->SetFillColor(42);
-   gStyle->SetOptFit(111);
-   hImpulse->DrawCopy();
-   //TCanvas::Update()
-   canvas1->cd(3)->Update();
-   canvas1->cd(3)->GetFrame()->SetFillColor(21);
-   canvas1->cd(3)->Modified();   
-///////////////////////////////////////////////////////////////////////////////////////
-   //create Second canvas2
-   TCanvas *canvas2 = new TCanvas();
-   canvas2->Divide(3);
-   canvas2->SetGridx();
-   canvas2->SetGridy();
-   canvas2->SetTitle("Comparison of Invariant Masses");
-   canvas2->SetFillColor(48);
-   
-   //canvas2 first part
-   canvas2->cd(1);
-   canvas2->cd(1)->SetFillColor(42);
+   ////////////////////////////////////////////////////////////////////
+   //create FIRST CANVAS with invariant masses results////////////////
+   ////////////////////////////////////////////////////////////////////
+   TCanvas *c1 = new TCanvas("c1","Generation Results",200,10,1400,900);
+   c1->SetFillColor(18);
+   auto pad1 = new TPad("pad1","typesGen",0.0125,0.50625,0.49375,0.9875,21);
+   auto pad2 = new TPad("pad2", "Impulse",0.0125,0.0125,0.49375,0.49375,21);
+   auto pad3 = new TPad("pad3",   "Theta",0.50625,0.50625,0.9875,0.9875,21);
+   auto pad4 = new TPad("pad4",     "Phi",0.50625,0.0125,0.9875,0.49375,21);
+   pad1->Draw();
+   pad2->Draw();
+   pad3->Draw();
+   pad4->Draw();
+
+   //working on pad1+++++++++++++++++types particle generated
+   pad1->cd();
+   pad1->SetGridx(); //setting a grid
+   pad1->SetGridy();
+   typesGenerated->SetLineColor(4); //histo cosmetics
+   typesGenerated->SetLineWidth(2);
+   typesGenerated->SetFillColorAlpha(38,50);
+   typesGenerated->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad1->Update();
+   pad1->GetFrame()->SetFillColor(42);
+   pad1->GetFrame()->SetBorderMode(-1);  
+   pad1->GetFrame()->SetBorderSize(5);
+   pad1->Modified();
+
+   //working on pad2+++++++++++++++++Impulse distribution
+   pad2->cd();
+   pad2->SetGridx(); //setting a grid
+   pad2->SetGridy();
+   hImpulse->SetLineColor(4); //histo cosmetics
+   hImpulse->SetFillColorAlpha(38,50);
+   hImpulse->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad2->Update();
+   pad2->GetFrame()->SetFillColor(42);
+   pad2->GetFrame()->SetBorderMode(-1);  
+   pad2->GetFrame()->SetBorderSize(5);
+   pad2->Modified();
+
+   //working on pad3+++++++++++++++++Theta distribution
+   pad3->cd();
+   pad3->SetGridx(); //setting a grid
+   pad3->SetGridy();
+   hTheta->SetLineColor(4);//histo cosmetics
+   hTheta->SetLineWidth(2);
+   hTheta->SetFillColorAlpha(38,50);
+   hTheta->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad3->Update();
+   pad3->GetFrame()->SetFillColor(42);
+   pad3->GetFrame()->SetBorderMode(-1);  
+   pad3->GetFrame()->SetBorderSize(5);
+   pad3->Modified();
+
+   //working on pad4+++++++++++++++++Phi distribution
+   pad4->cd();
+   pad4->SetGridx(); //setting a grid
+   pad4->SetGridy();
+   hPhi->SetLineColor(4);//histo cosmetics
+   hPhi->SetLineWidth(2);
+   hPhi->SetFillColorAlpha(38,50);
+   hPhi->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad4->Update();
+   pad4->GetFrame()->SetFillColor(42);
+   pad4->GetFrame()->SetBorderMode(-1);  
+   pad4->GetFrame()->SetBorderSize(5);
+   pad4->Modified();
+
+   ////////////////////////////////////////////////////////////////////
+   //create SECOND CANVAS with invariant masses results////////////////
+   ////////////////////////////////////////////////////////////////////
+   TCanvas *c2 = new TCanvas("c2","Comparison of Invariant Masses",200,10,1400,900);
+   c2->SetFillColor(18);
+   auto pad5 = new TPad("pad5","invMassSubtaction3-4",0.0125,0.50625,0.49375,0.9875,21);
+   auto pad6 = new TPad("pad6","invMassSubtaction1-2",0.0125,0.0125,0.49375,0.49375,21);
+   auto pad7 = new TPad("pad7",       "invMass decay",0.50625,0.50625,0.9875,0.9875,21);
+   //auto pad8 = new TPad("pad8","Theta",0.50625,0.0125,0.9875,0.49375,21);
+   pad5->Draw();
+   pad6->Draw();
+   pad7->Draw();
+   //pad8->Draw();
+
+   //working on pad5+++++++++++++++++invMassSubtaction3-4
+   pad5->cd();
+   pad5->SetGridx(); //setting a grid
+   pad5->SetGridy();
    subtraction_invMass_PK->GetXaxis()->SetTitle("Invariant Masses");
    subtraction_invMass_PK->GetYaxis()->SetTitle("Number of Particles");
-   subtraction_invMass_PK->GetYaxis()->SetTitleOffset(1.4); //move title from Yaxes of 1.4 cm  
-   //draw
-   gStyle->SetOptFit(111);
-   subtraction_invMass_PK->DrawCopy();
-   //TCanvas::Update()
-   canvas2->cd(1)->Update();
-   canvas2->cd(1)->GetFrame()->SetFillColor(21);
-   canvas2->cd(1)->Modified();
+   subtraction_invMass_PK->GetYaxis()->SetTitleOffset(1.3);
+   subtraction_invMass_PK->SetLineColor(4); //histo cosmetics
+   subtraction_invMass_PK->SetLineWidth(2);
+   subtraction_invMass_PK->SetFillColorAlpha(38,50);
+   subtraction_invMass_PK->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad5->Update();
+   pad5->GetFrame()->SetFillColor(42);
+   pad5->GetFrame()->SetBorderMode(-1);  
+   pad5->GetFrame()->SetBorderSize(5);
+   pad5->Modified();
 
-   //canvas2 second part
-   canvas2->cd(2);
-   canvas2->cd(2)->SetFillColor(42);
-   invMass_decayment->GetXaxis()->SetTitle("Invariant Masses");
-   invMass_decayment->GetYaxis()->SetTitle("Number of Particles");
-   invMass_decayment->GetYaxis()->SetTitleOffset(1.4); //move title from Yaxes of 1.4 cm
-   //draw 
-   gStyle->SetOptFit(111);
-   invMass_decayment->DrawCopy();
-   //TCanvas::Update()
-   canvas2->cd(2)->Update();
-   canvas2->cd(2)->GetFrame()->SetFillColor(21);
-   canvas2->cd(2)->Modified();
-   
-   //canvas2 third part
-   canvas2->cd(3);
-   canvas2->cd(3)->SetFillColor(42);
-   canvas2->cd(3)->GetFrame()->SetFillColor(21);
+   //working on pad6+++++++++++++++++invMassSubtaction1-2
+   pad6->cd();
+   pad6->SetGridx(); //setting a grid
+   pad6->SetGridy();
    subtraction_invMass->GetXaxis()->SetTitle("Invariant Masses");
    subtraction_invMass->GetYaxis()->SetTitle("Number of Particles");
-   subtraction_invMass->GetYaxis()->SetTitleOffset(1.4); //move title from Yaxes of 1.4 cm
-   //draw
-   gStyle->SetOptFit(111);
-   subtraction_invMass->DrawCopy();
-   //TCanvas::Update()
-   canvas2->cd(3)->Update();
-   canvas2->cd(3)->GetFrame()->SetFillColor(21);
-   canvas2->cd(3)->Modified(); 
-  
+   subtraction_invMass->GetYaxis()->SetTitleOffset(1.3);
+   subtraction_invMass->SetLineColor(4); //histo cosmetics
+   subtraction_invMass->SetLineWidth(2);
+   subtraction_invMass->SetFillColorAlpha(38,50);
+   subtraction_invMass->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad6->Update();
+   pad6->GetFrame()->SetFillColor(42);
+   pad6->GetFrame()->SetBorderMode(-1);  
+   pad6->GetFrame()->SetBorderSize(5);
+   pad6->Modified();
+
+   //working on pad7+++++++++++++++++invMass decay
+   pad7->cd();
+   pad7->SetGridx(); //setting a grid
+   pad7->SetGridy();
+   invMass_decayment->GetXaxis()->SetTitle("Invariant Masses");
+   invMass_decayment->GetYaxis()->SetTitle("Number of Particles");
+   invMass_decayment->GetYaxis()->SetTitleOffset(1.3);
+   invMass_decayment->SetLineColor(4); //histo cosmetics
+   invMass_decayment->SetLineWidth(2);
+   invMass_decayment->SetFillColorAlpha(38,50);
+   invMass_decayment->DrawCopy(); //draw
+   //inner pad cosmetics
+   pad7->Update();
+   pad7->GetFrame()->SetFillColor(42);
+   pad7->GetFrame()->SetBorderMode(-1);  
+   pad7->GetFrame()->SetBorderSize(5);
+   pad7->Modified();
+
    file->Close();
 }
